a1fb7a9b4b6b5e018fe633b067a068fa7bfbd2d1
alphuite <elias.frank99@gmail.com>
2016-08-21 21:44:35 +0200

    major bug fixes with being unable to connect to the chat

--- app.js
+++ app.js
@@ -1,65 +1,58 @@
 var tmi = require('tmi.js');
-var config = require('./configt.js');
+var config = require('./config.js')
 
 var options = {
-	options: {
-		debug = true
-	},
-	connection: {
-		cluster: "aws",
-		reconnect: true
-	},
-	identitiy: {
-		// Change in config.js
-		username: config.username,
-		password: config.password
-	},
-	channels: config.channel
+    options: {
+        debug: true
+    },
+    connection: {
+        cluster: "aws",
+        reconnect: true
+    },
+    identity: {
+        // Change in config file
+        username: config.username,
+        password: config.password
+    },
+    channels: config.channel
 };
 
 var client = new tmi.client(options);
-
 client.connect();
 
 client.on("chat", function (channel, userstate, message, self) {
-	if (self) return;
+    if (self) return;
 
-	if (channel == config.channel){
-		// Commands that only read from config.channel's channel
+	// Broadcaster only commands
+    if(userstate.username == config.broadname){
+        if(message == "!broadonly"){
+            client.say(config.broadname, "This is a broadcaster only command");
+        }
+    }
 
-		if (userstate.broadname == config.channel){
-			// Broadcaster only commands
-			if (message == "!broadonly"){
-				client.say(config.channel, "This is a broadcaster-only command!");
-			}
-		}
-
-		if (userstate.mod == true){
-			// Mod-Only Commands
-			if (message == "!modonly"){
-				client.say(config.channel, "This is a mod-only command!");
-			}
-
-		}
-		// Commands that can be used by everyone in the channel
-		if (message == !everyone){
-			client.say(config.channel, "This command can be used by anyone");
-		}
-	}
+	// Moderator commands
+    if(userstate.mod == true){
+        if(message == "!mondonly"){
+            client.say(config.broadname, "This is a mod-only command!");
+        }
+    }
+	// Commands that can be used by anyone
+    if(message == "!everyone"){
+        client.say(config.broadname, "This command can be used by anyone!");
+    }
 });
 
-client.on("connected", function (address, port){
+client.on("connected", function (address, port) {
+    console.log("BOT CONNECTED TO: " + address);
 
-	if (config.whiscon){
+	if(config.whiscon == true){
 		client.whisper(config.broadname, "Bot connected to the channel!");
 	}
-	
-	console.log("BOT CONNECTED TO: " + address);
 });
 
-client.on("disconnected", function (reason){
-	client.whisper(config.broadname, "Bot disconnected from the channel!");
-	console.log("BOT DISCONNECTED, REASON: " + reason);
+client.on("disconnected", function (reason) {
+    client.whisper(config.broadname, "Bot disconnected to the channel!");
+    console.log("BOT DISCONNECTED, REASON: " + reason);
 });
 
 client.on("whisper", function (from, userstate, message, self) {
@@ -67,9 +60,8 @@ client.on("whisper", function (from, userstate, message, self) {
 
 	if (self) return;
 
-	if (message == "!part" && userstate.username == config.broadname && config.whispart){
+	if (message == "!part" && userstate.username == config.broadname && config.whispart == true){
 		client.whisper(config.broadname, "Bot is going to disconnect, byebye!");
 		client.disconnect;
 	}
 });
-
