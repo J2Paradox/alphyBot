0c66ea16782a00d97a5abffb188f448262e1e146
alphuite <elias.frank99@gmail.com>
2016-08-27 23:50:19 +0200

    moved files - yo

--- moderation.js
+++ moderation.js
@@ -2,3 +2,7 @@ if (message.length > 140){
 client.timeout(config.broadname, userstate.username, 10, "Message too long");
 client.whisper(userstate.username, "You have been timed out because your message was too long.")
 }
+
+// if message contains link
+// client.timeout(config.broadname, userstate.username, 10, "Message contains link");
+// client.whisper(userstate.username, "You have been timed out because your message contained a hyperlink.")
