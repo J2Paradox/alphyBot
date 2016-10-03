f4c77ae53d89dd1c2742da87920d4efffcba9b9f
alphuite <elias.frank99@gmail.com>
2016-10-03 14:44:30 +0200

    Cleaned up the trash

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-// SERVER
-var alphyServer = require('./server.js');
-alphyServer.server()
-
 // IMPORTS
 const tmi = require('tmi.js');
-var config = require('./configs/config.json');
+const config = require('./configs/config.json');
+
+// CONSOLE LOG VARIABLES
+var dateData = String(new Date());
+var dateSplit = dateData.split(" ")
+var datePrint = dateSplit[1] + " " + dateSplit[2] + " " +
+    dateSplit[3] + " " + dateSplit[4]
+var dateL = "[" + datePrint + "] "
+const errorL = " [ ERROR ] "
+const infoL = " [ INFO ] "
 
 // TWITCH API CONFIG
 var twitchOptions = {
@@ -17,7 +22,7 @@ var twitchOptions = {
     },
     identity: {
         // CHANGE IN CONFIG ONLY
-        username: config.username,
+        username: config.botname,
         password: config.password
     },
     channels: config.channel
@@ -36,39 +41,23 @@ client.on("chat", function (channel, userstate, message, self) {
     commands.say(client, userstate, message);
 
     // EMOTE COUNTER
-    var emoteCounter = require('./modules/emote_counter.js')
-    emoteCounter.say(client, userstate, message);
+    // var emoteCounter = require('./modules/emote_counter.js')
+    // emoteCounter.say(client, userstate, message);
 
     // MODERATION
-    var moderation = require('./modules/moderation.js')
-    moderation.say(client, userstate, message);
+    // var moderation = require('./modules/moderation.js')
+    // moderation.say(client, userstate, message);
 
 });
 
 // CONSOLE LOG CONNTECTION
 client.on("connected", function (address, port) {
     console.log(dateL + infoL + "BOT CONNECTED TO: " + address);
-
-	if(config.whiscon == true){
-		client.whisper(config.broadname, "Bot connected to the channel!");
-	}
+    client.whisper(config.broadname, "Bot connected to the channel!");
 });
 
 // CONSOLE LOG DISCONNECT
 client.on("disconnected", function (reason) {
     client.whisper(config.broadname, "Bot disconnected from the channel!");
     console.log(dateL + errorL + "BOT DISCONNECTED, REASON: " + reason.toUpperCase());
-});
-
-// WHISPER CONTROL (MOVE AWAY FROM HERE SOON)
-client.on("whisper", function (from, userstate, message, self) {
-	if (self) return;
-
-    console.log(dateL + infoL + "WHISPER FROM: " + userstate.username + " MESSAGE: " + message)
-
-	if (message.toLowerCase() == "!part" && String(userstate.username) == config.broadname && config.whisppart == true){
-		client.whisper(config.broadname, "Bot is going to disconnect, byebye!");
-        console.log(dateL + infoL + "CLIENT WISPER DC")
-		client.disconnect();
-	}
 });
